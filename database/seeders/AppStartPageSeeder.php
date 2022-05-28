0d00aab3a82bc30c03d143cdd49f1fd40560ac20
Mohamed-Farh <mohamedfarh987@gmail.com>
2022-05-28 13:51:35 +0200

    Edit Seed

--- database/seeders/AppStartPageSeeder.php
+++ database/seeders/AppStartPageSeeder.php
@@ -14,9 +14,9 @@ class AppStartPageSeeder extends Seeder
      */
     public function run()
     {
-        AppStartPage::create(['text_ar' => 'هذا النص الاول هو مثال لنص يمكن أن يستبدل في نفس المساحة .',    'text_en' => 'First Lorem ipsum dolor sit amet, consectetur adipiscing elit.',  'text_ur' => 'First Lorem ipsum dolor sit amet, consectetur adipiscing elit.',     'image' => 'images/category/clothes.jpg',           'status' => true, 'number' => '1' ]);
-        AppStartPage::create(['text_ar' => 'هذا النص الثاني هو مثال لنص يمكن أن يستبدل في نفس المساحة .',  'text_en' => 'Second Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'text_ur' => 'Second Lorem ipsum dolor sit amet, consectetur adipiscing elit.',    'image' => 'images/category/womenclothes.jpg',      'status' => true, 'number' => '2' ]);
-        AppStartPage::create(['text_ar' => 'هذا النص الثالث هو مثال لنص يمكن أن يستبدل في نفس المساحة .',  'text_en' => 'Third Lorem ipsum dolor sit amet, consectetur adipiscing elit.',  'text_ur' => 'Third Lorem ipsum dolor sit amet, consectetur adipiscing elit.',     'image' => 'images/category/womenclothes2.jpg',     'status' => true, 'number' => '3' ]);
+        AppStartPage::create(['text_ar' => 'هذا النص الاول هو مثال لنص يمكن أن يستبدل في نفس المساحة .',    'text_en' => 'First Lorem ipsum dolor sit amet, consectetur adipiscing elit.',  'text_ur' => 'یہ پہلا متن ایک متن کی ایک مثال ہے جسے ایک ہی جگہ میں تبدیل کیا جا سکتا ہے۔',     'image' => 'images/category/clothes.jpg',           'status' => true, 'number' => '1' ]);
+        AppStartPage::create(['text_ar' => 'هذا النص الثاني هو مثال لنص يمكن أن يستبدل في نفس المساحة .',  'text_en' => 'Second Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'text_ur' => 'یہ پہلا متن ایک متن کی ایک مثال ہے جسے ایک ہی جگہ میں تبدیل کیا جا سکتا ہے۔',    'image' => 'images/category/womenclothes.jpg',      'status' => true, 'number' => '2' ]);
+        AppStartPage::create(['text_ar' => 'هذا النص الثالث هو مثال لنص يمكن أن يستبدل في نفس المساحة .',  'text_en' => 'Third Lorem ipsum dolor sit amet, consectetur adipiscing elit.',  'text_ur' => 'یہ پہلا متن ایک متن کی ایک مثال ہے جسے ایک ہی جگہ میں تبدیل کیا جا سکتا ہے۔',     'image' => 'images/category/womenclothes2.jpg',     'status' => true, 'number' => '3' ]);
 
     }
 }
--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -14,10 +14,10 @@ class UnitSeeder extends Seeder
      */
     public function run()
     {
-        Unit::create(['name_ar' => 'كيلوجرام',  'name_en' => 'Kg',      'name_ur' => 'Kg',      'status' => true ]);
-        Unit::create(['name_ar' => 'لتر',       'name_en' => 'Liter',   'name_ur' => 'Liter',   'status' => true ]);
-        Unit::create(['name_ar' => 'قطعة',      'name_en' => 'Piece',   'name_ur' => 'Piece',   'status' => true ]);
-        Unit::create(['name_ar' => 'كرتونة',    'name_en' => 'Carton',  'name_ur' => 'Carton',  'status' => true ]);
+        Unit::create(['name_ar' => 'كيلوجرام',  'name_en' => 'Kg',      'name_ur' => 'کلو',    'status' => true ]);
+        Unit::create(['name_ar' => 'لتر',       'name_en' => 'Liter',   'name_ur' => 'لیٹر',   'status' => true ]);
+        Unit::create(['name_ar' => 'قطعة',      'name_en' => 'Piece',   'name_ur' => 'ٹکڑا',   'status' => true ]);
+        Unit::create(['name_ar' => 'كرتونة',    'name_en' => 'Carton',  'name_ur' => 'کارٹن',  'status' => true ]);
 
     }
 }
